1f356ddc9e853a96226292eb70278cafa8635dc9
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-04-30 16:56:10 -0700

    Rename a few testing scripts

--- single-chunk-test.sh
+++ single-chunk-test.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 # Export a single chunk to local parquet files
-export-chunks-test.sh
+export-single-chunk.sh
 
 # Upload a single chunk to GCS (should trigger cloud function)
-upload-chunks-test.sh
+upload-single-chunks.sh
