20990caf93fedfcef435f6e00eb592465731edd3
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-07-18 12:55:42 -0700

    Remove old script

--- chunk-test-single.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-# Export a single chunk to local parquet files
-export-single-chunk.sh
-
-# Upload a single chunk to GCS (should trigger cloud function)
-upload-single-chunk.sh
