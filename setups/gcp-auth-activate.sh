50bb0a6bdb96b79fd3d19af1369a90724e431bf5
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-07-21 12:09:05 -0700

    Remove some simple setup scripts

--- setups/gcp-auth-activate.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-gcloud auth activate-service-account --key-file="$HOME/.gcp/keys/ppdb-storage-manager-key.json"
--- setups/gcp-credentials-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-GOOGLE_APPLICATION_CREDENTIALS="$HOME/.gcp/keys/ppdb-storage-manager-key.json"
-if [ ! -f "${GOOGLE_APPLICATION_CREDENTIALS}" ]; then
-  echo "Error: Service account key file not found: ${GOOGLE_APPLICATION_CREDENTIALS}"
-else
-  echo "Using service account key file: ${GOOGLE_APPLICATION_CREDENTIALS}"
-  export GOOGLE_APPLICATION_CREDENTIALS
-fi
