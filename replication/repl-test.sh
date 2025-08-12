5c000f24639215482960fe59ec2f9d97b0450114
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-08-12 13:14:30 -0700

    Default to one chunk export

--- replication/repl-test.sh
+++ replication/repl-test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ $# -ne 1 ]] || ! [[ "$1" =~ ^[0-9]+$ ]]; then
-    echo "Usage: $0 <number_of_times>"
+count="${1:-1}"
+
+if ! [[ "$count" =~ ^[0-9]+$ ]]; then
+    echo "Usage: $0 [number_of_times]"
     exit 1
 fi
-
-count="$1"
 
 for ((i = 1; i <= count; i++)); do
     echo "Export run $i of $count"
