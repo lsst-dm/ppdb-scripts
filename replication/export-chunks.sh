abe8063efb7509a7e274c3b0334670a43fe522f1
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-08-18 10:24:39 -0700

    Set variable for monitoring

--- replication/export-chunks.sh
+++ replication/export-chunks.sh
@@ -2,13 +2,13 @@
 
 set -euxo pipefail
 
-ppdb-replication \
+DAX_APDB_MONITOR_CONFIG="logging:lsst.dax.ppdb.metrics,lsst.dax.ppdb.export._chunk_exporter,-any" \
+    ppdb-replication \
     --log-level ${LOG_LEVEL} \
     export-chunks \
     $APDB_CONFIG_FILE $PPDB_CONFIG_FILE \
     --directory $PPDB_STAGING_DIR \
     --compression snappy \
-    --batch-size 100000 \
     --min-wait-time 0 \
     --max-wait-time 0 \
     --check-interval 0 \
