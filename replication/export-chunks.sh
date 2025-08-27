c4e477c4401298f22410fa19e90199b68a3f6a83
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-08-27 14:10:26 -0700

    Update replication test scripts

--- replication/export-chunks.sh
+++ replication/export-chunks.sh
@@ -2,16 +2,13 @@
 
 set -euxo pipefail
 
-DAX_APDB_MONITOR_CONFIG="logging:lsst.dax.ppdb.metrics,lsst.dax.ppdb.export._chunk_exporter,-any" \
-    ppdb-replication \
-    --log-level ${LOG_LEVEL} \
-    export-chunks \
+# --log-level ${LOG_LEVEL} \
+
+DAX_APDB_MONITOR_CONFIG="logging:lsst.dax.ppdb.metrics,lsst.dax.ppdb.bigquery._chunk_exporter,-any" \
+    ppdb-replication run \
     $APDB_CONFIG_FILE $PPDB_CONFIG_FILE \
-    --directory $PPDB_STAGING_DIR \
-    --compression snappy \
     --min-wait-time 0 \
     --max-wait-time 0 \
     --check-interval 0 \
     --exit-on-empty \
-    --delete-existing \
     "$@"
--- replication/upload-chunks.sh
+++ replication/upload-chunks.sh
@@ -2,15 +2,12 @@
 
 set -euxo pipefail
 
-DAX_APDB_MONITOR_CONFIG="logging:lsst.dax.ppdb.metrics,lsst.dax.ppdb.export._chunk_uploader,-any" \
+DAX_APDB_MONITOR_CONFIG="logging:lsst.dax.ppdb.metrics,lsst.dax.ppdb.bigquery._chunk_uploader,-any" \
     ppdb-replication \
     --log-level ${LOG_LEVEL} \
     upload-chunks \
-    --bucket ${GCS_BUCKET} \
-    --dataset ${GCP_PROJECT}:${DATASET_ID} \
     --wait-interval 0 \
     --upload-interval 0 \
-    --prefix data \
     --exit-on-empty \
     --exit-on-error \
     ${PPDB_CONFIG_FILE}
